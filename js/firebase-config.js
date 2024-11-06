09805a227a52eabf5bebb5d4713b49f838ba1a7b
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-11-06 21:03:42 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,12 @@
 // firebase-config.js
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAXTwuy5ACKCRCO4M18_v8f_Bwie2gRIeU",
+  authDomain: "mr-gadgets.firebaseapp.com",
+  projectId: "mr-gadgets",
+  storageBucket: "mr-gadgets.firebasestorage.app",
+  messagingSenderId: "144245989215",
+  appId: "1:144245989215:web:71e02602c87bf0087aa0ea"
 };
+
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
